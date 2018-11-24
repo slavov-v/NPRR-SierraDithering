9ed2706868f6de7407776e8257ab983915cff9fc
Vasil Slavov <v.slavov96@gmail.com>
2018-11-24 21:49:02 +0200

    Add `SierraDithering` project

---
+++ SierraDithering/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SierraDithering
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.imgContainer = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgContainer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // imgContainer
+            // 
+            this.imgContainer.Location = new System.Drawing.Point(30, 106);
+            this.imgContainer.Name = "imgContainer";
+            this.imgContainer.Size = new System.Drawing.Size(661, 526);
+            this.imgContainer.TabIndex = 1;
+            this.imgContainer.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(818, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Apply Sierra Lite";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(818, 301);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Load Original";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(818, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Apply Sierra Two-Row";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(818, 92);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Apply Sierra";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(178, 31);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(163, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Do not preserve colors";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 644);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imgContainer);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgContainer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox imgContainer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ SierraDithering/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SierraDithering
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap OriginalImage = null;
+        public Bitmap NewImage = null;
+        public bool BlackAndWhite = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            imgContainer.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+
+        public int applyThreshold(int value)
+        {
+            return value < 128 ? 0 : 255;
+        }
+
+        public int normalizeColorByte(int colorByte)
+        {
+            if (colorByte < 0) return 0;
+            if (colorByte > 255) return 255;
+
+            return colorByte;
+        }
+
+        public Color getFilteredColor(int x, int y, double divisor, int errorRed, int errorGreen, int errorBlue)
+        {
+            int red = normalizeColorByte(NewImage.GetPixel(x, y).R + (int)Math.Round(errorRed * divisor));
+            int green = normalizeColorByte(NewImage.GetPixel(x, y).G + (int)Math.Round(errorGreen * divisor));
+            int blue = normalizeColorByte(NewImage.GetPixel(x, y).B + (int)Math.Round(errorBlue * divisor));
+            
+            return Color.FromArgb(red, green, blue);
+
+        }
+
+        public void mutateIndexSafely(int x, int y, double divisor, int errorRed, int errorGreen, int errorBlue)
+        {
+            if ((x < NewImage.Width && x >= 0) && (y < NewImage.Height && y >= 0 ))
+            {
+                Color color = getFilteredColor(x, y, divisor, errorRed, errorGreen, errorBlue);
+                NewImage.SetPixel(x, y, color);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "Select Image";
+            fileDialog.ShowDialog();
+
+            OriginalImage = new Bitmap(fileDialog.FileName);
+
+            imgContainer.Image = OriginalImage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NewImage = new Bitmap(OriginalImage);
+
+            if (OriginalImage == null)
+            {
+                MessageBox.Show("Load an image first");
+                return;
+            }
+
+            for (int y = 0; y < NewImage.Height; y++)
+            {
+                for (int x = 0; x < NewImage.Width; x++)
+                {
+                    Color currentPixel = NewImage.GetPixel(x, y);
+
+                    int redOld = currentPixel.R;
+                    int greenOld = currentPixel.G;
+                    int blueOld = currentPixel.B;
+
+                    int redNew = 0;
+                    int greenNew = 0;
+                    int blueNew = 0;
+
+                    if (!BlackAndWhite)
+                    {
+                        redNew = applyThreshold(redOld);
+                        greenNew = applyThreshold(greenOld);
+                        blueNew = applyThreshold(blueOld);
+                    }
+                    else
+                    {
+                        int gray = (int)(0.299 * currentPixel.R + 0.587 * currentPixel.G + 0.114 * currentPixel.B);
+                        int newBit = gray < 128 ? 0 : 255;
+
+                        redNew = newBit;
+                        greenNew = newBit;
+                        blueNew = newBit;
+                    }
+
+                    NewImage.SetPixel(x, y, Color.FromArgb(redNew, greenNew, blueNew));
+
+                    int redError = redOld - redNew;
+                    int greenError = greenOld - greenNew;
+                    int blueError = blueOld - blueNew;
+
+                    mutateIndexSafely(x + 1, y, (double)2 / (double)4, redError, greenError, blueError);
+                    mutateIndexSafely(x - 1, y + 1, (double)1 / (double)4, redError, greenError, blueError);
+                    mutateIndexSafely(x, y + 1, (double)1 / (double)4, redError, greenError, blueError);
+                }
+            }
+
+            imgContainer.Image = NewImage;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(OriginalImage == null)
+            {
+                MessageBox.Show("No Image loaded");
+                return;
+            }
+
+            imgContainer.Image = OriginalImage;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            NewImage = new Bitmap(OriginalImage);
+
+            if (OriginalImage == null)
+            {
+                MessageBox.Show("Load an image first");
+                return;
+            }
+
+            for (int y = 0; y < NewImage.Height; y++)
+            {
+                for (int x = 0; x < NewImage.Width; x++)
+                {
+                    Color currentPixel = NewImage.GetPixel(x, y);
+
+                    int redOld = currentPixel.R;
+                    int greenOld = currentPixel.G;
+                    int blueOld = currentPixel.B;
+
+                    int redNew = 0;
+                    int greenNew = 0;
+                    int blueNew = 0;
+
+                    if (!BlackAndWhite)
+                    {
+                        redNew = applyThreshold(redOld);
+                        greenNew = applyThreshold(greenOld);
+                        blueNew = applyThreshold(blueOld);
+                    }
+                    else
+                    {
+                        int gray = (int)(0.299 * currentPixel.R + 0.587 * currentPixel.G + 0.114 * currentPixel.B);
+                        int newBit = gray < 128 ? 0 : 255;
+
+                        redNew = newBit;
+                        greenNew = newBit;
+                        blueNew = newBit;
+                    }
+
+                    NewImage.SetPixel(x, y, Color.FromArgb(redNew, greenNew, blueNew));
+
+                    int redError = redOld - redNew;
+                    int greenError = greenOld - greenNew;
+                    int blueError = blueOld - blueNew;
+
+                    mutateIndexSafely(x + 1, y, (double)4 / (double)16, redError, greenError, blueError);
+                    mutateIndexSafely(x + 2, y, (double)3 / (double)16, redError, greenError, blueError);
+                    mutateIndexSafely(x - 2, y + 1, (double)1 / (double)16, redError, greenError, blueError);
+                    mutateIndexSafely(x - 1, y + 1, (double)2 / (double)16, redError, greenError, blueError);
+                    mutateIndexSafely(x, y + 1, (double)3 / (double)16, redError, greenError, blueError);
+                    mutateIndexSafely(x + 1, y + 1, (double)2 / (double)16, redError, greenError, blueError);
+                    mutateIndexSafely(x + 2, y + 1, (double)1 / (double)16, redError, greenError, blueError);
+                }
+            }
+
+            imgContainer.Image = NewImage;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            NewImage = new Bitmap(OriginalImage);
+
+            if (OriginalImage == null)
+            {
+                MessageBox.Show("Load an image first");
+                return;
+            }
+
+            for (int y = 0; y < NewImage.Height; y++)
+            {
+                for (int x = 0; x < NewImage.Width; x++)
+                {
+                    Color currentPixel = NewImage.GetPixel(x, y);
+
+                    int redOld = currentPixel.R;
+                    int greenOld = currentPixel.G;
+                    int blueOld = currentPixel.B;
+
+                    int redNew = 0;
+                    int greenNew = 0;
+                    int blueNew = 0;
+
+                    if(!BlackAndWhite)
+                    {
+                        redNew = applyThreshold(redOld);
+                        greenNew = applyThreshold(greenOld);
+                        blueNew = applyThreshold(blueOld);
+                    }else
+                    {
+                        int gray = (int)(0.299 * currentPixel.R + 0.587 * currentPixel.G + 0.114 * currentPixel.B);
+                        int newBit = gray < 128 ? 0 : 255;
+
+                        redNew = newBit;
+                        greenNew = newBit;
+                        blueNew = newBit;
+                    }
+
+                    NewImage.SetPixel(x, y, Color.FromArgb(redNew, greenNew, blueNew));
+
+                    int redError = redOld - redNew;
+                    int greenError = greenOld - greenNew;
+                    int blueError = blueOld - blueNew;
+
+                    mutateIndexSafely(x + 1, y, (double)5 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x + 2, y, (double)3 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x - 2, y + 1, (double)2 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x - 1, y + 1, (double)4 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x, y + 1, (double)5 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x + 1, y + 1, (double)4 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x + 2, y + 1, (double)2 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x - 1, y + 2, (double)2 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x, y + 2, (double)3 / (double)32, redError, greenError, blueError);
+                    mutateIndexSafely(x + 1, y + 2, (double)2 / (double)32, redError, greenError, blueError);
+                }
+            }
+
+            imgContainer.Image = NewImage;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            BlackAndWhite = !BlackAndWhite;
+
+            button6.Text = BlackAndWhite ? "Preserve colors" : "Do not preserve colors";
+
+        }
+    }
+}
